355438bc20cf2ba83a95671a9e339718b567cfc5
Tobi Williams <tobiwilliams003@gmail.com>
2025-02-12 20:38:52 +0100

    Refined BlogHerobody: fixed alignment & spacing issues

--- src/components/Herobody/BlogHerobody.tsx
+++ src/components/Herobody/BlogHerobody.tsx
@@ -3,11 +3,17 @@ import { lato_font } from '../../styles/font'
 
 const BlogHerobody = () => {
   return (
-    <div className="w-full phone:h-full phone:justify-center phone:items-center phone:flex xl:h-full xl:justify-center xl:items-center xl:flex" >
-      <div className="text-center" >
-          <p className={`mx-auto text-[#FFFDF6] xl:pt-52 pt-8 md:pt-14 ${lato_font.className} md:text-[60px] text-[28px] xl:text-[100px] text-center md:max-w-[513px] xl:max-w-[734px] font-bold xl:tracking-[-1px] md:tracking-[-0.6px] tracking-[-0.28px] xl:leading-[110px] md:leading-[72px] leading-[32px] phone:max-w-[230px]`} >Our Blog</p>
-          <p className='mx-auto text-[#E59297] font-semibold text-[12px] md:text-[18px] xl:text-[36px] xl:pt-4 pt-2 md:pt-5' >Discover, Learn, Inspire</p>
-          <div className="w-full flex justify-center pb-20 md:pb-40 xl:pb-88 xl:pt-11 pt-3" ></div>
+    <div className="w-full flex justify-center items-center min-h-screen px-4">
+      <div className="text-center w-full max-w-screen-lg">
+        <p className={`mx-auto text-[#FFFDF6] xl:pt-52 pt-8 md:pt-14 ${lato_font.className} md:text-[60px] text-[28px] xl:text-[100px] text-center max-w-[90%] md:max-w-[513px] xl:max-w-[734px] font-bold xl:tracking-[-1px] md:tracking-[-0.6px] tracking-[-0.28px] xl:leading-[110px] md:leading-[72px] leading-[36px]`}>
+          Our Blog
+        </p>
+
+        <p className="mx-auto text-[#E59297] font-semibold text-[14px] md:text-[18px] xl:text-[36px] xl:pt-4 pt-2 md:pt-5">
+          Discover, Learn, Inspire
+        </p>
+
+        <div className="w-full flex justify-center pb-12 md:pb-24 xl:pb-28 xl:pt-11 pt-3"></div>
       </div>
     </div>
   )
