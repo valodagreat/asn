746853bb8f73ba3705150127bc275a57e3ff6454
Tobi Williams <tobiwilliams003@gmail.com>
2025-02-12 20:38:08 +0100

    Updated BlogSectionHome: improved layout & responsive styles

--- src/components/BlogSectionHome/BlogSectionHome.tsx
+++ src/components/BlogSectionHome/BlogSectionHome.tsx
@@ -9,6 +9,7 @@ import { PageButtonLoader } from '../Button/buttonload';
 const BlogSectionHome = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [ optionsList, setOptionsList ] = useState<[]| any[]>([]);
+
   useEffect(() => {
     setIsLoading(true);
     const q = query(collection(getFirestore(firebase_app), 'Article'));
@@ -24,16 +25,25 @@ const BlogSectionHome = () => {
       unsubscribe();
     };
   }, [])
+
   return (
-    <div className='md:px-[80px] px-7 xl:px-[120px]' >
-        <h1 className={`text-[#CC2630] font-medium xl:text-[56px] md:text-[42px] text-[16px] ${manual.className} xl:pt-[150px] pt-8 md:pt-[60px] phoneTab:pb-[6px] phone:pb-[10px]`} >Our Blog</h1>
-        {isLoading ? <div className='h-screen w-screen flex justify-center items-center' >
-                <div className='h-[60px] w-[60px]' >
-                    <PageButtonLoader />
-                </div>
-            </div>  : <div className='xl:pb-[121px] pb-[40px] md:pb-[64px] flex flex-wrap phoneTab:flex-col xl:gap-x-12 phone:gap-x-[19px]' >
-            {optionsList?.map((option)=> <BlogCard key={option?.id} data={option} />)}
-        </div>}
+    <div className='px-4 sm:px-7 md:px-20 lg:px-[80px] xl:px-[120px]'>
+        <h1 className={`text-[#CC2630] font-medium text-base md:text-[42px] xl:text-[56px] ${manual.className} pt-8 md:pt-[60px] xl:pt-[150px] pb-[10px] md:pb-[6px]`}>
+          Our Blog
+        </h1>
+        {isLoading ? (
+          <div className='fixed inset-0 flex justify-center items-center'>
+            <div className='h-[60px] w-[60px]'>
+              <PageButtonLoader />
+            </div>
+          </div>
+        ) : (
+          <div className='pb-10 md:pb-16 xl:pb-[121px] flex flex-wrap gap-5 xl:gap-12'>
+            {optionsList?.map((option) => (
+              <BlogCard key={option?.id} data={option} />
+            ))}
+          </div>
+        )}
     </div>
   )
 }
